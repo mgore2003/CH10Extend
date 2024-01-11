5d44d829ea7e790d614e0ff5994b9c226967c60f
mgore2003 <142228045+mgore2003@users.noreply.github.com>
2024-01-11 08:46:33 -0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+/*
+    Student Name: Mark Gore
+    File Name: script.js
+    Date: 01/11/2024
+*/
+
+$(function(){
+	$("#hide").click(function(){
+		$("p").hide();
+	});
+	
+	$("#show").click(function(){
+		$("p").show();
+	});
+}
